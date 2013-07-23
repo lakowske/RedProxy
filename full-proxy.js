6b9b0aa0d60da47f878ce5a935240218d2d4b025
Seth Lakowske <lakowske@gmail.com>
2013-07-23 00:51:46 -0500

    added an initial proxy implementation

---
+++ full-proxy.js
@@ -0,0 +1,14 @@
+var http = require('http'),
+httpProxy = require('http-proxy');
+
+var options = {
+  pathnameOnly:true,
+  router: {
+    '/camera': '10.0.0.11:8080/?action=stream',
+    '': 'localhost:9999'
+  }
+};
+//
+// Create your proxy server
+//
+httpProxy.createServer(options).listen(8000);
